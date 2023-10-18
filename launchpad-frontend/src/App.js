0a3e6a471a83120943601d34de4728bf0ff79a9b
Fatima Jawed <fatimajawed@fatimas-mbp.wifi.int.ryerson.ca>
2023-10-18 11:30:14 -0400

    made changes tpp app.js

--- launchpad-frontend/src/App.js
+++ launchpad-frontend/src/App.js
@@ -1,25 +1,19 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Login from "./pages/login";
+import SignUp from "./pages/register";
+import './App.css';
+
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Router>
+          <Routes>
+            <Route exact path ='/' element={<Login />}/>
+            <Route path ='/signup' element={<SignUp />}/>
+            </Routes>
+      </Router>
     </div>
   );
 }
-
 export default App;
