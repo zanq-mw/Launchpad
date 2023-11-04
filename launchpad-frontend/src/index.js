e8621d228f51583bf51dd62297fec52357cf59da
Zan Qureshi <zanq.mw@gmail.com>
2023-11-04 15:06:26 -0400

    Moved login and signup to startup.js

--- launchpad-frontend/src/index.js
+++ launchpad-frontend/src/index.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { NavBar } from "./navigation";
-import App from "./App";
+import Startup from "./startup";
 import reportWebVitals from "./reportWebVitals";
 import { LandingPage } from "./landingPage/landingPage";
 
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Pages />
-    {/* <App /> */}
+    {/* <Startup /> */}
   </React.StrictMode>
 );
 
