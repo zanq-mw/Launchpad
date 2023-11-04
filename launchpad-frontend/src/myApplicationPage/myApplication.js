f9860c9d6d6a2adc36504a1299ead4f35906e92b
Brian Nguyen <brian.b.nguyen99@gmail.com>
2023-11-03 20:30:04 -0400

    Separapte mock data into it's own file

--- launchpad-frontend/src/myApplicationPage/myApplication.js
+++ launchpad-frontend/src/myApplicationPage/myApplication.js
@@ -9,56 +9,7 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-
-// NOTE: mock data will be delete once data connected to db
-// mock data for app rows. 
-const mockApplicationData = [
-      {
-        title: "Software Developer, Intern",
-        company: "Wealthsimple",
-        duration: "4-month Internship",
-        location: "Remote",
-        status: "Pending",
-        logo: "https://images.ctfassets.net/v44fuld738we/3p54yem0uWnzJSPyCLdQgN/10e0569c130b369cf6b33e2f1a88acc7/_2019_Wealthsimple_Favicon_Black.png",
-        date: "2023-10-21",
-        action: "Complete",
-        imageText:"Wealthsimple Logo"
-      },
-      {
-        title: "Data Scientist, Intern",
-        company: "Scotiabank",
-        duration: "8-months Internship",
-        location: "Toronto, ON",
-        status: "Pending",
-        logo: "https://seeklogo.com/images/S/scotiabank-logo-D2F1AF87B5-seeklogo.com.png",
-        date: "2023-10-21",
-        action: "pending",
-        imageText:"Scotiabank Logo"
-      },
-      {
-        title: "Software Engineer, Intern",
-        company: "Amazon",
-        duration: "New Grad",
-        location: "Vancouver, BC",
-        status: "Pending",
-        logo: "https://seeklogo.com/images/A/amazon-icon-logo-8F577E5C31-seeklogo.com.png",
-        date: "2023-10-21",
-        action: "Complete",
-        imageText:"Amazon Logo"
-      },
-      {
-        title: "Software Engineer, Junior",
-        company: "Microsoft",
-        duration: "New Grad",
-        location: "Toronto, ON",
-        status: "Pending",
-        logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Microsoft_logo.svg/2048px-Microsoft_logo.svg.png",
-        date: "2023-10-21",
-        action: "Complete",
-        imageText:"Logo",
-        redirect:"app_link"
-      },
-]
+import { mockApplicationData } from "./mockData";
 
 // top banner
 function UserApplicationsInfo(){
@@ -107,6 +58,7 @@ function UserApplications(){
 
     return(
         <Card sx = {MyApplicationStyles.cardMargin}>
+        <div style={{ marginTop: '20px' }}>
             <TableContainer>
                 <Table>
                     <TableHead>
@@ -193,6 +145,8 @@ function UserApplications(){
                     </TableBody>
                 </Table>
             </TableContainer>
+            </div>
+
         </Card>
     );
 }
