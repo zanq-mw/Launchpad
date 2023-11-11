43e0f42951bb307db2dcdbd546c369d947c3ad6e
Alyanna Santos <alyannamariesantos@gmail.com>
2023-11-11 17:22:18 -0500

    Use database information for page and pop-ups

--- launchpad-frontend/src/components/editButton.js
+++ launchpad-frontend/src/components/editButton.js
@@ -1,144 +1,135 @@
-import React, { useState } from 'react';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import Typography from '@mui/material/Typography';
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Typography from "@mui/material/Typography";
 
-export function EditButton (){
-
+export function EditButton(props) {
+  const data = props.data;
   const [isClicked, setisClicked] = useState(false);
 
   const clickedOpen = () => {
     setisClicked(true);
- };
+  };
 
   const clickedClose = () => {
     setisClicked(false);
   };
 
-    return (
-        <div>
-          <Button onClick={clickedOpen} >
-            <img
-            src={require("../images/accountEdit.png")}
-            height={"35px"}
-            alt="edit"
-            ></img>
-          </Button>
+  return (
+    <div>
+      <Button onClick={clickedOpen}>
+        <img
+          src={require("../images/accountEdit.png")}
+          height={"35px"}
+          alt="edit"
+          fullWidth={true}
+        ></img>
+      </Button>
 
-          {/* popup code below */}
-          <Dialog open={isClicked} onClose={clickedClose} sx ={EditButtonStyles.popup} >
-            <Typography variant="h5" component="div" sx ={EditButtonStyles.popupContainer}>
+      {/* popup code below */}
+      <Dialog
+        open={isClicked}
+        onClose={clickedClose}
+        sx={EditButtonStyles.popup}
+        maxWidth="sm"
+        fullWidth={true}
+      >
+        <Typography
+          variant="h5"
+          component="div"
+          sx={EditButtonStyles.popupContainer}
+        >
+          <DialogTitle sx={EditButtonStyles.mainTitle}>
+            {data && data.title}
+          </DialogTitle>
 
-              <DialogTitle sx = {EditButtonStyles.mainTitle}>Address</DialogTitle>
+          <DialogContent>
+            <DialogContentText sx={EditButtonStyles.subTitle}>
+              {data && data.subtitle}
+            </DialogContentText>
+          </DialogContent>
 
-              <DialogContent>
-                <DialogContentText sx = {EditButtonStyles.subTitle}>
-                  Edit the address, postal code, and province/state you currently reside at
-                </DialogContentText>
-              </DialogContent>
-
-              <DialogContent sx ={EditButtonStyles.popupForm}>
-
+          <DialogContent sx={EditButtonStyles.popupForm}>
+            {data &&
+              data.fields.map((field) => (
                 <TextField
                   autoFocus
                   margin="dense"
                   id="address"
                   InputLabelProps={{
                     style: {
-                      color: 'black',    
-                      fontSize: '20px',
-                      fontWeight: 'bold',
+                      color: "black",
+                      fontSize: "20px",
+                      fontWeight: "bold",
                     },
                     shrink: true, // move the label to the top
                   }}
-                  label="Address"
-                  placeholder="123 Nevada Street"
+                  label={field.label}
+                  placeholder={field.placeholder}
                   type="text"
                   fullWidth
                   variant="standard"
                 />
-                <TextField
-                  autoFocus
-                  margin="dense"
-                  id="postal"
-                  InputLabelProps={{
-                    style: {
-                      color: 'black',    
-                      fontSize: '20px',
-                      fontWeight: 'bold',
-                    },
-                    shrink: true, // move the label to the top
-                  }}
-                  label="Postal Code"
-                  placeholder="M5K 2K9"
-                  type="text"
-                  fullWidth
-                  variant="standard"
-                />
-                <TextField
-                  autoFocus
-                  margin="dense"
-                  id="name"
-                  InputLabelProps={{
-                    style: {
-                      color: 'black',    
-                      fontSize: '20px',
-                      fontWeight: 'bold',
-                    },
-                    shrink: true, // move the label to the top
-                  }}
-                  label="Province/State"
-                  placeholder="ON"
-                  type="text"
-                  fullWidth
-                  variant="standard"
-                />
-              
-              </DialogContent>
-              <DialogActions>
-                <Button variant="contained" size="small" sx={EditButtonStyles.cancelButton} onClick={clickedClose}>Cancel</Button>
-                <Button variant="contained" size="small" sx={EditButtonStyles.saveButton} onClick={clickedClose}>Save</Button>
-              </DialogActions>
-            </Typography>
-          </Dialog>
-        </div>
-      );
+              ))}
+          </DialogContent>
+          <DialogActions>
+            <Button
+              variant="contained"
+              size="small"
+              sx={EditButtonStyles.cancelButton}
+              onClick={clickedClose}
+            >
+              Cancel
+            </Button>
+            <Button
+              variant="contained"
+              size="small"
+              sx={EditButtonStyles.saveButton}
+              onClick={clickedClose}
+            >
+              Save
+            </Button>
+          </DialogActions>
+        </Typography>
+      </Dialog>
+    </div>
+  );
 }
 
 const EditButtonStyles = {
-
-  popupContainer:{
-      background: "#FFF",
-      height: "100%"
+  popupContainer: {
+    background: "#FFF",
+    height: "100%",
+    width: "100%",
   },
   popupForm: {
-      background: '#F7F7F7',
-    },
+    background: "#F7F7F7",
+  },
   mainTitle: {
-      color: '#5E17EB',
-      fontFamily: 'League Spartan',
-      fontSize: 32,
-      fontWeight: 700,
-      paddingBottom: "0",
-      marginBottom: "0"
-    },
+    color: "#5E17EB",
+    fontFamily: "League Spartan",
+    fontSize: 32,
+    fontWeight: 700,
+    paddingBottom: "0",
+    marginBottom: "0",
+  },
   subTitle: {
-    color: '#393939',
-    fontFamily: 'Roboto',
+    color: "#393939",
+    fontFamily: "Roboto",
     fontSize: 15,
     fontWeight: 700,
   },
   saveButton: {
-    background: '#5E17EB'
+    background: "#5E17EB",
   },
   cancelButton: {
-    color: '#5E17EB',
-    background: '#F7F7F7',
-    border: '2px solid #5E17EB'
-  }
-}
+    color: "#5E17EB",
+    background: "#F7F7F7",
+    border: "2px solid #5E17EB",
+  },
+};
