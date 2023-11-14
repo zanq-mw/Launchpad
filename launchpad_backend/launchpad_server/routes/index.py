855c292d4eb71558785cb021c6760839069b11f4
zafrinn <123769836+zafrinn@users.noreply.github.com>
2023-11-14 12:01:29 -0500

    Add files via upload

--- launchpad_backend/launchpad_server/routes/index.py
+++ launchpad_backend/launchpad_server/routes/index.py
@@ -4,19 +4,22 @@ from .forms import SignupForm, LoginForm
 from flask_login import login_user
 from flask_bcrypt import Bcrypt
 from flask_pymongo import PyMongo
+from flask_cors import CORS
+from flask_bcrypt import check_password_hash
+from flask import session  
 import pymongo
 import sys
-import re
-sys.path.append('./launchpad_server/routes')
+sys.path.append('.\\launchpad_server\\routes')
 from startup_data import startup_data
-
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/database'
 
 # Initialize the PyMongo extension
 mongo = PyMongo(app)
-
-
 bcrypt = Bcrypt(app)
+
+
+CORS(app, origins='*')
+
 
 def setup_db():
     for data in startup_data:
@@ -44,51 +47,6 @@ def get_applications(user_id):
         doc.pop("_id")
     return (jsonify({"data": result}))   
 
-@app.route("/jobs")
-def get_jobs():
-    # Select * from posting where postingTitle LIKE user_id
-
-    # get all of the query strings
-    type_filter = request.args.get('type')
-    duration_filter = request.args.get('duration')
-    location_filter = request.args.get('location')
-    # transform everything to queryable strings
-    everything_regex = re.compile(".*")
-    type_regex = everything_regex if type_filter == "null" else type_filter 
-    duration_regex = everything_regex if duration_filter == "null" else duration_filter 
-    location_regex = everything_regex if location_filter == "null" else location_filter
-    # access database
-    collection = mongo.db.posting
-    query = {"type": type_regex, "duration": duration_regex, "workModel": location_regex}
-    result = list(collection.find(query))
-
-    for doc in result:
-        doc.pop("_id")
-    return (jsonify({"data": result}))
-
-@app.route("/companies")
-def get_companies():
-    # Select * from company
-    
-    # access database
-    collection = mongo.db.company
-    query = {}
-    result = list(collection.find(query))
-
-    for doc in result:
-        doc.pop("_id")
-    return (jsonify({"data": result}))   
-
-@app.route("/users/<int:user_id>")
-def get_users(user_id):
-    # Select * from users where userId=user_id
-    collection = mongo.db.user
-    query = {"userId": user_id}
-    result = list(collection.find(query))
-
-    for doc in result:
-        doc.pop("_id")
-    return (jsonify({"data": result}))   
 
 @app.route("/check_db")
 def check_db():
@@ -106,8 +64,28 @@ def index():
 def landing(form):
         return render_template('landing.html',title='Landing Page',form=form)
 
-@app.route("/register", methods = ['POST', 'GET'])
+@app.route("/api/signup", methods = ['POST', "GET"])
 def register():
+    if request.method == 'POST':
+        data = request.get_json()
+        print(data)
+        fname =  data.get('lname')
+        lname =data.get('lname')
+        year = data.get('year')
+        program = data.get('program')
+        username = data.get('username')
+        password = data.get('password')
+        print(
+            "First Name: ", fname,
+            "\nLast Name: ", lname,
+            "\nYear: ", year,
+            "\nProgram ", program,
+            "\nusername: ", username,
+            "\npasssword: ", password)
+    
+    response = {'message': 'User registered successfully'}
+    return (response)
+    """
     form = SignupForm()
     if form.validate_on_submit():
         hash_pass =  bcrypt.generate_password_hash(request.form['password']).decode('utf-8')
@@ -115,16 +93,48 @@ def register():
         if check_pass:
             return landing(form)
     return render_template('register.html',title='SignUp', form=form)
+    """
 
-@app.route("/login", methods=['GET', 'POST'])
+
+@app.route("/api/login", methods=['POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        email = request.form['email']
-        password =  request.form['password']
-        return f'Hello, {email}. Your password is {password}.'  
-    else:
-        print('something wrong')
-        flash('Login failed. Please check your email and password.', 'danger')
-    return render_template('login.html', title='Login', form=form)
+    if request.method == 'POST':
+        data = request.get_json()
+        print(data)
+        username = data.get('username')
+        password = data.get('password')
+        print(
+            "Username: ", username,
+            "\nPassword: ", password)
 
+        
+        user_data = mongo.db.get_collection("user").find_one({"email": username})
+
+        if user_data:
+            for key, value in user_data.items():
+               #print(f"{key}: {value}")
+               if key == "password":
+                if value == password:
+                    stored_password = user_data.get("password")
+                    rehashed_stored_password = bcrypt.generate_password_hash(stored_password).decode('utf-8') #To check if the hashed password is taken
+                    if bcrypt.check_password_hash(rehashed_stored_password, password):
+                        user_id = user_data.get("userId") 
+                        session['user_id'] = str(user_id)
+
+                        user_info = {
+                            "userId": user_data.get("userId"),
+                            "email": user_data.get("email"),
+                            "firstName": user_data.get("firstName"),
+                        }
+
+                        response = {
+                                "message": "User Logged In successfully",
+                                "user_info": user_info  
+                        }
+                        return jsonify(response)
+        else:
+            response = {"message": "Incorrect Password"}
+            return jsonify(response)
+
+        response = {"message": "Incorrect Password"}
+        return jsonify(response)
