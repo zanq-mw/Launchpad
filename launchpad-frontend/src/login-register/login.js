327a7fde3c4204ce9561674812a3d4bf252e7c9e
zafrinn <123769836+zafrinn@users.noreply.github.com>
2023-11-14 11:52:44 -0500

    Add files via upload

--- launchpad-frontend/src/login-register/login.js
+++ launchpad-frontend/src/login-register/login.js
@@ -1,45 +1,93 @@
 import React from "react";
 import logo from "../images/launchpadLogo.png";
 import { NavLink, Link } from "react-router-dom";
+import  Combined from "../combined.js";
+import { BrowserRouter as Router} from "react-router-dom"
+import {root} from "../root.js"
 
 class Login extends React.Component {
+  handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = {
+      username: e.target.username.value,
+      password: e.target.Password.value
+    };
+    console.log(formData);
+
+    const response = await fetch('/api/login', {
+      method: 'POST',
+      dataType: "json",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization"
+      },
+      body: JSON.stringify(formData),      
+    });
+
+    const responseData = await response.json();
+    console.log(responseData);
+
+    if (responseData.message === "User Logged In successfully") {
+      root.render(
+        <React.StrictMode>
+         <Router>
+            { <Combined/> } 
+         </Router>
+          
+          {/* <Startup /> */}
+        </React.StrictMode>
+      );
+      //window.location.href = "/landingPage";
+    } else if (responseData.message === 'Incorrect Password') {
+      alert("Incorrect Password");
+    } else {
+      console.log("email already in use");
+    }
+ 
+  };
+
   render() {
     return (
-      <div class="container-main">
-        <div class="container-left">
-          <div class="container-logo">
-            <img class="startup-logo" src={logo} alt="Logo" />
+      <div className="container-main">
+        <div className="container-left">
+          <div className="container-logo">
+            <img className="startup-logo" src={logo} alt="Logo" />
             <p id="slogan">
               Launching Careers, One <br></br>Opportunity at a Time
             </p>
           </div>
         </div>
 
-        <div class="container-right">
-          <div class="heading-container2">Welcome Back</div>
+        <div className="container-right">
+          <div className="heading-container2">Welcome Back</div>
 
-          <div class="form-container-login">
-            <form action="" method="POST">
-              <div class="input2">
+          <div className="form-container-login">
+            <form onSubmit={this.handleSubmit}>
+              <div className="input2">
                 <label class="label">Email</label>
                 <input
-                  class="input"
+                  className="input"
                   type="text"
                   name="username"
                   placeholder="example@yourschool.com"
+                  required
                 />
               </div>
-              <div class="input2">
-                <label class="label">Password</label>
+              <div className="input2">
+                <label className="label">Password</label>
                 <input
-                  class="input"
+                  className="input"
                   type="password"
                   name="Password"
                   placeholder="***********"
+                  required 
                 />
               </div>
-              <div class="btn-cont">
-                <button class="submit-btn">Log In</button>
+              <div className="btn-cont">
+                <button className="submit-btn">Log In</button>
               </div>
             </form>
             <p>
@@ -61,3 +109,4 @@ class Login extends React.Component {
   }
 }
 export default Login;
+
