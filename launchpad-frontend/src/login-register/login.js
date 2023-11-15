927984479d470edfd4c51768f4d2efebbaa3f8d8
Alyanna Santos <alyannamariesantos@gmail.com>
2023-11-14 19:43:30 -0500

    Remove Login handleSubmit (out of scope)

--- launchpad-frontend/src/login-register/login.js
+++ launchpad-frontend/src/login-register/login.js
@@ -1,90 +1,73 @@
 import React from "react";
 import logo from "../images/launchpadLogo.png";
-import { NavLink, Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import "./startup.css";
+import { useNavigate } from "react-router-dom";
 
-class Login extends React.Component {
-  handleSubmit = async (e) => {
-    e.preventDefault();
+function Login() {
+  const navigate = useNavigate();
 
-    const formData = {
-      username: e.target.username.value,
-      password: e.target.Password.value
-    };
-    console.log(formData);
-
-    const response = await fetch('/api/login', {
-      method: 'POST',
-      dataType: "json",
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
-        "Access-Control-Allow-Headers": "Content-Type, Authorization"
-      },
-      body: JSON.stringify(formData),      
-    });
-
-    const responseData = await response.json();
-    console.log(responseData);
-  };
-
-  render() {
-    return (
-      <div className="container-main">
-        <div className="container-left">
-          <div className="container-logo">
-            <img className="startup-logo" src={logo} alt="Logo" />
-            <p id="slogan">
-              Launching Careers, One <br></br>Opportunity at a Time
-            </p>
-          </div>
+  return (
+    <div className="container-main">
+      <div className="container-left">
+        <div className="container-logo">
+          <img className="startup-logo" src={logo} alt="Logo" />
+          <p id="slogan">
+            Launching Careers, One <br></br>Opportunity at a Time
+          </p>
         </div>
+      </div>
 
-        <div className="container-right">
-          <div className="heading-container2">Welcome Back</div>
+      <div className="container-right">
+        <div className="heading-container2">Welcome Back</div>
 
-          <div className="form-container-login">
-            <form onSubmit={this.handleSubmit}>
-              <div className="input2">
-                <label className="label">Email</label>
-                <input
-                  className="input"
-                  type="text"
-                  name="username"
-                  placeholder="example@yourschool.com"
-                  required
-                />
-              </div>
-              <div className="input2">
-                <label className="label">Password</label>
-                <input
-                  className="input"
-                  type="password"
-                  name="Password"
-                  placeholder="***********"
-                  required 
-                />
-              </div>
-              <div className="btn-cont">
-                <button className="submit-btn">Log In</button>
-              </div>
-            </form>
-            <p>
-              <span id="forgot-password">
-                {" "}
-                <i>Forgot password?</i>
-              </span>
-            </p>
-            <div id="login-container-login">
-              <p>Already have an account?&nbsp;</p>
-              <NavLink to="/signup">
-                <i>Register Here</i>
-              </NavLink>
+        <div className="form-container-login">
+          <form>
+            <div className="input2">
+              <label className="label">Email</label>
+              <input
+                className="input"
+                type="text"
+                name="username"
+                placeholder="example@yourschool.com"
+                required
+              />
             </div>
+            <div className="input2">
+              <label className="label">Password</label>
+              <input
+                className="input"
+                type="password"
+                name="Password"
+                placeholder="***********"
+                required
+              />
+            </div>
+            <div className="btn-cont">
+              <button
+                className="submit-btn"
+                onClick={() => navigate("/landing")}
+              >
+                Log In
+              </button>
+            </div>
+          </form>
+          <p>
+            <span id="forgot-password">
+              {" "}
+              <i>Forgot password?</i>
+            </span>
+          </p>
+          <div id="login-container-login">
+            <p>Already have an account?&nbsp;</p>
+            <NavLink to="/signup">
+              <i>Register Here</i>
+            </NavLink>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
 export default Login;
