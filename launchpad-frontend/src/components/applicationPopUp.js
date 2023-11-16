37f37bdaad5a5a1159003053eac924f0c1865c95
Alyanna Santos <alyannamariesantos@gmail.com>
2023-11-16 10:39:10 -0500

    Minor changes to pop-up styling

--- launchpad-frontend/src/components/applicationPopUp.js
+++ launchpad-frontend/src/components/applicationPopUp.js
@@ -1,23 +1,17 @@
-import React, { useState } from 'react';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import Typography from '@mui/material/Typography';
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import Typography from "@mui/material/Typography";
 import "@fontsource/league-spartan";
 
-
-
-
-export function ApplyButton ({ companyName }){
-
+export function ApplyButton({ companyName }) {
   const [open, setOpen] = useState(false);
   const [resumeFile, setResumeFile] = useState(null);
   const [coverLetterFile, setCoverLetterFile] = useState(null);
-  const [submissionStatus, setSubmissionStatus] = useState('pending'); // 'pending', 'success'
+  const [submissionStatus, setSubmissionStatus] = useState("pending"); // 'pending', 'success'
   const [resumeUploadDate, setResumeUploadDate] = useState(null);
   const [coverLetterUploadDate, setCoverLetterUploadDate] = useState(null);
-
-  
 
   const handleFileChange = (event, fileType) => {
     const fileInput = event.target;
@@ -26,12 +20,14 @@ export function ApplyButton ({ companyName }){
     if (fileInput.files.length > 0) {
       const file = fileInput.files[0];
       // Set the corresponding file state based on the fileType
-      fileType === 'resume' ? setResumeFile(file) && setResumeUploadDate(currentDate) : setCoverLetterFile(file) && setCoverLetterUploadDate(currentDate); 
-      
-
+      fileType === "resume"
+        ? setResumeFile(file) && setResumeUploadDate(formattedDate)
+        : setCoverLetterFile(file) && setCoverLetterUploadDate(formattedDate);
     } else {
       // If no files are selected, reset the corresponding file state
-      fileType === 'resume' ? setResumeFile(null) && setResumeUploadDate(null) : setCoverLetterFile(null) && setCoverLetterUploadDate(null); ;
+      fileType === "resume"
+        ? setResumeFile(null) && setResumeUploadDate(null)
+        : setCoverLetterFile(null) && setCoverLetterUploadDate(null);
     }
   };
 
@@ -45,240 +41,286 @@ export function ApplyButton ({ companyName }){
   const handleSubmission = () => {
     // Check if both resume and cover letter files are uploaded
     if (resumeFile && coverLetterFile) {
-    // For example, you can send them to the server using an API call
+      // For example, you can send them to the server using an API call
 
-    // Update the submission status to indicate success
-      setSubmissionStatus('success');
-      console.log('Submission successful');
-
+      // Update the submission status to indicate success
+      setSubmissionStatus("success");
+      console.log("Submission successful");
     } else {
       // Display an error or prompt the user to upload both files
-      console.error('Please upload both resume and cover letter files.');
+      console.error("Please upload both resume and cover letter files.");
     }
   };
+
   const handleOpen = () => {
     resetForm();
     setOpen(true);
- };
+  };
 
   const handleClose = () => {
     setOpen(false);
   };
+
   const resetForm = () => {
     setResumeFile(null);
     setCoverLetterFile(null);
-    setSubmissionStatus('pending');
+    setSubmissionStatus("pending");
   };
-  
-    return (
-        <div>
-          <Button  onClick={handleOpen} variant='contained' sx={ApplyButtonStyles.apply} >
-            Apply
-          </Button>
-          <Dialog open={open} onClose={handleClose} fullWidth maxWidth={'lg'}sx={ApplyButtonStyles.popupSize} >
-          {submissionStatus === 'pending' ? (
-            <>
-            <div style={{ marginTop: '60px' }}/>
-              <Typography sx={ApplyButtonStyles.header}>
-                Apply to {companyName}
-              </Typography>
-              <div style={{ marginBottom: '10px' }} />
-              <Typography sx={ApplyButtonStyles.heading2}>
-                  Please upload your resume and cover letter
-              <div style={{ marginBottom: '45px' }} />
-              <div style={{ clear: 'both' }} />
-                  <div style={ApplyButtonStyles.greyBox}>
-                      <div style={ApplyButtonStyles.greyBoxText}>
-                          Resume
-                      </div>
-                      <div style={{ marginBottom: '10px' }} />
-                      <div style={ApplyButtonStyles.buttonStyles}>
-                      <div style={ApplyButtonStyles.buttonContainerStyles}>
-                          <span style={ApplyButtonStyles.labelStyles}>PDF</span>
-                          <span style={ApplyButtonStyles.fileNameStyles}>{resumeFile ? resumeFile.name : 'No file selected'}</span>
-                          <p style={ApplyButtonStyles.descriptionStyles}>
-                              Uploaded on {resumeUploadDate || 'No date available'}
-                          </p>
 
-                      </div>
-                      
-                  </div>
-                  <div style={ApplyButtonStyles.uploadContainer}>
-                      <input type="file" id="resumeInput" accept=".pdf" style={{ display: 'none' }} onChange={(event) => handleFileChange(event, 'resume')} />
-                      <button style={ApplyButtonStyles.uploadbuttonStyles} onClick={() => handleUploadButtonClick('resume')}>
-                          Upload Resume
-                      </button>
-                      
-                  </div>
-                  <div style={ApplyButtonStyles.greyBox}>
-                      <div style={ApplyButtonStyles.greyBoxText}>
-                          Cover Letter
-                      </div>
-                      <div style={{ marginBottom: '10px' }} />
-                      <div style={ApplyButtonStyles.buttonStyles}>
-                      <div style={ApplyButtonStyles.buttonContainerStyles}>
-                          <span style={ApplyButtonStyles.labelStyles}>PDF</span>
-                          <span style={ApplyButtonStyles.fileNameStyles}> {coverLetterFile ? coverLetterFile.name : 'No file selected'}</span>
-                          <p style={ApplyButtonStyles.descriptionStyles}>
-                              Uploaded on {coverLetterUploadDate || 'No date available'}
-                          </p>
-                      </div>        
-                  </div>
-                  <div style={ApplyButtonStyles.uploadContainer}>
-                      <input type="file" id="coverLetterInput" accept=".pdf" style={{ display: 'none' }} onChange={(event) => handleFileChange(event, 'coverLetter')} />
-                      <button style={ApplyButtonStyles.uploadbuttonStyles} onClick={() => handleUploadButtonClick('coverLetter')}>
-                          Upload Cover Letter
-                      </button>
-                  </div>
-                  </div>
-              </div>
-              <div style={{ marginBottom: '45px' }} />              
-                <DialogActions>
-                  <Button variant="contained" size="small" sx={ApplyButtonStyles.cancelButton} onClick={handleClose}>Cancel</Button>
-                  <Button variant="contained" size="small" sx={ApplyButtonStyles.saveButton} button onClick={handleSubmission} disabled={!resumeFile || !coverLetterFile}>Submit</Button>
-                </DialogActions>
-              </Typography>
-            </>
-          ): (
-            <>
-            <div style={{ marginTop: '60px' }}/>
+  return (
+    <div>
+      <Button
+        onClick={handleOpen}
+        variant="contained"
+        sx={ApplyButtonStyles.apply}
+      >
+        Apply
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        fullWidth
+        maxWidth={"lg"}
+        sx={ApplyButtonStyles.popupSize}
+      >
+        {submissionStatus === "pending" ? (
+          <>
+            <div style={{ marginTop: "60px" }} />
             <Typography sx={ApplyButtonStyles.header}>
-                  Apply to {companyName}
+              Apply to {companyName}
             </Typography>
-            <div style={{ marginBottom: '10px' }} />
+            <div style={{ marginBottom: "10px" }} />
+            <Typography>
+              <Typography sx={ApplyButtonStyles.heading2}>
+                Please upload your resume and cover letter
+              </Typography>
+              <div style={{ marginBottom: "45px" }} />
+              <div style={{ clear: "both" }} />
+              <div style={ApplyButtonStyles.greyBox}>
+                <div style={ApplyButtonStyles.greyBoxText}>Resume</div>
+                <div style={{ marginBottom: "10px" }} />
+                <div style={ApplyButtonStyles.buttonStyles}>
+                  <div style={ApplyButtonStyles.buttonContainerStyles}>
+                    <span style={ApplyButtonStyles.labelStyles}>PDF</span>
+                    <span style={ApplyButtonStyles.fileNameStyles}>
+                      {resumeFile ? resumeFile.name : "No file selected"}
+                    </span>
+                    <p style={ApplyButtonStyles.descriptionStyles}>
+                      Uploaded on {resumeUploadDate || "No date available"}
+                    </p>
+                  </div>
+                </div>
+                <div style={ApplyButtonStyles.uploadContainer}>
+                  <input
+                    type="file"
+                    id="resumeInput"
+                    accept=".pdf"
+                    style={{ display: "none" }}
+                    onChange={(event) => handleFileChange(event, "resume")}
+                  />
+                  <button
+                    style={ApplyButtonStyles.uploadbuttonStyles}
+                    onClick={() => handleUploadButtonClick("resume")}
+                  >
+                    Upload Resume
+                  </button>
+                </div>
+                <div style={ApplyButtonStyles.greyBox}>
+                  <div style={ApplyButtonStyles.greyBoxText}>Cover Letter</div>
+                  <div style={{ marginBottom: "10px" }} />
+                  <div style={ApplyButtonStyles.buttonStyles}>
+                    <div style={ApplyButtonStyles.buttonContainerStyles}>
+                      <span style={ApplyButtonStyles.labelStyles}>PDF</span>
+                      <span style={ApplyButtonStyles.fileNameStyles}>
+                        {" "}
+                        {coverLetterFile
+                          ? coverLetterFile.name
+                          : "No file selected"}
+                      </span>
+                      <p style={ApplyButtonStyles.descriptionStyles}>
+                        Uploaded on{" "}
+                        {coverLetterUploadDate || "No date available"}
+                      </p>
+                    </div>
+                  </div>
+                  <div style={ApplyButtonStyles.uploadContainer}>
+                    <input
+                      type="file"
+                      id="coverLetterInput"
+                      accept=".pdf"
+                      style={{ display: "none" }}
+                      onChange={(event) =>
+                        handleFileChange(event, "coverLetter")
+                      }
+                    />
+                    <button
+                      style={ApplyButtonStyles.uploadbuttonStyles}
+                      onClick={() => handleUploadButtonClick("coverLetter")}
+                    >
+                      Upload Cover Letter
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div style={{ margin: "20px" }} />
+              <DialogActions>
+                <Button
+                  variant="contained"
+                  size="small"
+                  sx={ApplyButtonStyles.cancelButton}
+                  onClick={handleClose}
+                >
+                  Cancel
+                </Button>
+                <Button
+                  variant="contained"
+                  size="small"
+                  sx={ApplyButtonStyles.saveButton}
+                  button
+                  onClick={handleSubmission}
+                  disabled={!resumeFile || !coverLetterFile}
+                >
+                  Submit
+                </Button>
+              </DialogActions>
+            </Typography>
+          </>
+        ) : (
+          <>
+            <div style={{ marginTop: "60px" }} />
+            <Typography sx={ApplyButtonStyles.header}>
+              Apply to {companyName}
+            </Typography>
+            <div style={{ marginBottom: "10px" }} />
             <Typography sx={ApplyButtonStyles.heading2}>
-                  Your application has succesfully been submitted!
+              Your application has succesfully been submitted!
             </Typography>
-            <div style={{ marginBottom: '45px' }} />
-            <div style={{ clear: 'both' }} />           
+            <div style={{ marginBottom: "45px" }} />
+            <div style={{ clear: "both" }} />
           </>
         )}
-          </Dialog>
-        </div>
-      );
+      </Dialog>
+    </div>
+  );
 }
 
 const ApplyButtonStyles = {
-    
-    apply: {
-        backgroundColor: "#5e17eb",
-        boxShadow: "none",
-        borderRadius: "15px",
-        "&:hover": {
-          backgroundColor: "#D3D3D3",
-          boxShadow: "none",
-        },
-      },
-
-
-  header:{
-    fontFamily: 'League Spartan', 
-    fontSize: '38px', 
-    color: '#5E17EB', 
-    marginLeft: '50px', 
-    fontWeight: 'bold'
-  },
-  heading2:{
-    fontFamily: 'League Spartan', 
-    fontSize: '18px', 
-    color: '#393939', 
-    marginLeft: '50px',
-    marginBottom: '45px',
-    fontWeight: 'bold' 
-  },
-
-  greyBox:{
-    background: '#F7F7F7',
-    padding: '10px 0',
-    margin: '-10px 0',
-    height: '90%',
-    width: '90%',
-    
-  },
-  greyBoxText:{
-    paddingLeft: '20px'
-  },
-
-  subTitle:{
-    fontFamily: 'League Spartan', 
-    fontSize: '18px',
-     fontWeight: 'bold', 
-     color: '#00000', 
-     marginLeft: '100px'
-  },
-
-  popupForm: {
-      background: '#F7F7F7',
+  apply: {
+    backgroundColor: "#5e17eb",
+    boxShadow: "none",
+    borderRadius: "15px",
+    "&:hover": {
+      backgroundColor: "#D3D3D3",
+      boxShadow: "none",
     },
- 
+  },
+
+  header: {
+    fontFamily: "League Spartan",
+    fontSize: "38px",
+    color: "#5E17EB",
+    marginLeft: "50px",
+    fontWeight: "bold",
+  },
+  heading2: {
+    fontFamily: "League Spartan",
+    fontSize: "18px",
+    color: "#393939",
+    marginLeft: "50px",
+    marginBottom: "45px",
+    fontWeight: "bold",
+  },
+  greyBox: {
+    background: "#F7F7F7",
+    paddingTop: "20px",
+    paddingBottom: "20px",
+    height: "100%",
+    width: "100%",
+  },
+  greyBoxText: {
+    paddingLeft: "100px",
+    paddingTop: "5px",
+    paddingBottom: "5px",
+    fontWeight: "bold",
+  },
+  subTitle: {
+    fontFamily: "League Spartan",
+    fontSize: "18px",
+    fontWeight: "bold",
+    color: "#00000",
+    marginLeft: "100px",
+  },
+  popupForm: {
+    background: "#F7F7F7",
+  },
   saveButton: {
-    background: '#5E17EB'
+    background: "#5E17EB",
   },
   cancelButton: {
-    color: '#5E17EB',
-    background: '#F7F7F7',
-    border: '2px solid #5E17EB'
+    color: "#5E17EB",
+    background: "#F7F7F7",
+    border: "2px solid #5E17EB",
   },
-   buttonStyles: {
-    display: 'flex',
-    alignItems: 'center',
-    marginLeft: '100px',
-    marginBottom: '10px',
-    cursor: 'pointer', // Add cursor style to indicate it's clickable
-  },
-  
-  buttonContainerStyles: {
-    borderRadius: '20px',
-    padding: '10px',
-    backgroundColor: 'white',
-    width: '700px',
-    border: '1px solid black',
-    background: 'linear-gradient(to right, #5E17EB 8%, white 8%)',
-    position: 'relative',
-    overflow: 'hidden', // Hide overflowing content
-  },
-  
-   labelStyles: {
-    color: 'white',
-    fontWeight: 'bold',
-    marginLeft: '4px',
-    position: 'relative',
-    top: '16px',
-  },
-  
-  fileNameStyles: {
-    marginLeft: '23px',
-    fontWeight: 'bold',
-    position: 'relative',
-    top: '10px',
-    overflow: 'hidden', // Hide overflowing content
-    whiteSpace: 'nowrap', // Prevent wrapping
-    textOverflow: 'ellipsis', // Show ellipsis (...) for long filenames
-  },
-  
-descriptionStyles: {
-    fontSize: '12px',
-    marginTop: '5px',
-    color: 'rgba(0, 0, 0, 0.42)',
-    marginLeft: '60px',
-    position: 'relative',
-    top: '5px',
+  buttonStyles: {
+    display: "flex",
+    alignItems: "center",
+    marginLeft: "100px",
+    marginBottom: "10px",
+    cursor: "pointer", // Add cursor style to indicate it's clickable
   },
 
-  uploadbuttonStyles:{
-    fontFamily: 'League Spartan',
-    fontWeight: 'bold',
-    fontSize: '16px',
-    backgroundColor: '#F7F7F7',
-    color: '#5E17EB',
-    border: '2px solid #5E17EB',
-    padding: '10px 20px',
-    cursor: 'pointer',
-    borderRadius: '20px',
+  buttonContainerStyles: {
+    borderRadius: "20px",
+    padding: "10px",
+    backgroundColor: "white",
+    width: "700px",
+    border: "1px solid black",
+    background: "linear-gradient(to right, #5E17EB 8%, white 8%)",
+    position: "relative",
+    overflow: "hidden", // Hide overflowing content
   },
-  uploadContainer:{
-    display: 'flex',
-    alignItems: 'center',
-    marginLeft: '100px',
-  }
-}
+
+  labelStyles: {
+    color: "white",
+    fontWeight: "bold",
+    marginLeft: "4px",
+    position: "relative",
+    top: "16px",
+  },
+
+  fileNameStyles: {
+    marginLeft: "23px",
+    fontWeight: "bold",
+    position: "relative",
+    top: "10px",
+    overflow: "hidden", // Hide overflowing content
+    whiteSpace: "nowrap", // Prevent wrapping
+    textOverflow: "ellipsis", // Show ellipsis (...) for long filenames
+  },
+
+  descriptionStyles: {
+    fontSize: "12px",
+    marginTop: "5px",
+    color: "rgba(0, 0, 0, 0.42)",
+    marginLeft: "60px",
+    position: "relative",
+    top: "5px",
+  },
+
+  uploadbuttonStyles: {
+    fontFamily: "League Spartan",
+    fontWeight: "bold",
+    fontSize: "16px",
+    backgroundColor: "#F7F7F7",
+    color: "#5E17EB",
+    border: "2px solid #5E17EB",
+    padding: "10px 20px",
+    cursor: "pointer",
+    borderRadius: "20px",
+  },
+  uploadContainer: {
+    display: "flex",
+    alignItems: "center",
+    marginLeft: "100px",
+  },
+};
