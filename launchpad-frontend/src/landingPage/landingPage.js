454eb5882298d643031f92a041bf609a161ea6e0
Zan Qureshi <zanq.mw@gmail.com>
2023-11-24 21:23:40 -0500

    Added recommended table endpoint and used data for landing page

--- launchpad-frontend/src/landingPage/landingPage.js
+++ launchpad-frontend/src/landingPage/landingPage.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Grid, IconButton } from "@mui/material";
-import { mock_data } from "./mockData";
+import { Grid } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import "./landingPage.css";
 import { SubmittedIcon, ViewedIcon } from "../components/landingIcons";
@@ -9,9 +8,17 @@ import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableRow from "@mui/material/TableRow";
-import Bookmark from "@mui/icons-material/Bookmark";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
 
-
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#5E17EB",
+    },
+  },
+});
 
 function StatusIcon(status) {
   if (status === "Applied") {
@@ -24,53 +31,72 @@ function StatusIcon(status) {
 }
 
 export function LandingPage({ userId, setPage }) {
-  const [data, setData] = useState(mock_data);
-  const [appsExpanded, setAppsExpanded] = useState(false);
-  const [savedExpanded, setSavedExpanded] = useState(false);
   const [userData, setUserData] = useState({});
   const [applicationData, setApplicationData] = useState({});
   const [jobData, setJobData] = useState({});
   const [companyData, setCompanyData] = useState({});
+  const [recommendedData, setRecommendedData] = useState({});
+  const [loading1, setLoading1] = useState(true);
+  const [loading2, setLoading2] = useState(true);
   const navigate = useNavigate();
   useEffect(() => {
     if (userId) {
-      fetch(`/users/${userId}`)
-        .then((res) => res.json())
-        .then((data) => {
-          setUserData(data);
-        });
+      const userPromise = fetch(`/users/${userId}`).then((res) => res.json());
+      const applicationsPromise = fetch(`/applications/${userId}`).then((res) =>
+        res.json()
+      );
+      const recommendedJobsPromise = fetch(`/recommended-jobs/${userId}`).then(
+        (res) => res.json()
+      );
+
+      Promise.all([userPromise, applicationsPromise, recommendedJobsPromise])
+        .then(([userData, applicationData, recommendedData]) => {
+          setUserData(userData);
+          setApplicationData(applicationData);
+          setRecommendedData(recommendedData);
+          setLoading1(false);
+        })
+        .catch((error) => console.error("Error fetching user data:", error));
     }
   }, [userId]);
 
   useEffect(() => {
-    if (userId) {
-      fetch(`/applications/${userId}`)
-        .then((res) => res.json())
-        .then((data) => {
-          setApplicationData(data);
-        });
-    }
-  }, [userId]);
+    const jobsPromise = fetch(
+      `/jobs?type=null&duration=null&location=null`
+    ).then((res) => res.json());
+    const companiesPromise = fetch(`/companies`).then((res) => res.json());
 
-  useEffect(() => {
-    fetch(`/jobs?type=null&duration=null&location=null`)
-      .then((res) => res.json())
-      .then((data) => {
-        setJobData(data);
-      });
-  }, []);
-
-  useEffect(() => {
-    fetch(`/companies`)
-      .then((res) => res.json())
-      .then((data) => {
-        setCompanyData(data);
-      });
+    Promise.all([jobsPromise, companiesPromise])
+      .then(([jobData, companyData]) => {
+        setJobData(jobData);
+        setCompanyData(companyData);
+        setLoading2(false);
+      })
+      .catch((error) =>
+        console.error("Error fetching job and company data:", error)
+      );
   }, []);
 
   const jobsCompanyData =
     jobData.data && companyData.data && userData.data
       ? jobData.data.map((job) => {
+          const companyRecord = companyData.data.find(
+            (company) => company.companyId === job.companyId
+          );
+          const saved = userData.data[0].savedPostings.find(
+            (posting) => posting.postingId === job.postingId
+          );
+          return {
+            ...job,
+            companyName: companyRecord.companyName,
+            saved: saved ? true : false,
+          };
+        })
+      : [];
+
+  const recommendedCompanyData =
+    recommendedData.data && companyData.data && userData.data
+      ? recommendedData.data.map((job) => {
           const companyRecord = companyData.data.find(
             (company) => company.companyId === job.companyId
           );
@@ -99,30 +125,35 @@ export function LandingPage({ userId, setPage }) {
 
   const savedPostings = jobsCompanyData.filter((job) => job.saved);
 
-  const recommendedPostings = jobsCompanyData.sort((a, b) => {
+  const recommendedPostings = recommendedCompanyData.sort((a, b) => {
     return new Date(a.deadline) - new Date(b.deadline);
   });
-
-  function handleSave(i) {
-    // CHANGE THIS
-    let temp = { ...data };
-    temp.recommended[i].saved = !temp.recommended[i].saved;
-    setData(temp);
-  }
 
   const clickedJob = (jobId) => {
     setPage(`/jobs/${jobId}`);
     navigate(`/jobs/${jobId}`);
   };
 
-
-  if (!userId) {
-    return <div>Loading...</div>;
+  if (loading1 || loading2) {
+    return (
+      <ThemeProvider theme={theme}>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100vh",
+          }}
+        >
+          <CircularProgress />
+        </Box>
+      </ThemeProvider>
+    );
   }
 
   const handleSeeMoreClick = () => {
-    setPage('/applications');
-    navigate('/applications');
+    setPage("/applications");
+    navigate("/applications");
   };
 
   return (
@@ -133,110 +164,106 @@ export function LandingPage({ userId, setPage }) {
       <Grid container spacing={6}>
         <Grid item xs={8}>
           <p style={PageStyles.headings}>Applications</p>
-<TableContainer>
-  <Table aria-label="simple table" style={PageStyles.table}>
-    <TableBody>
-      {applicationsEnhanced
-        .slice(0, appsExpanded ? applicationsEnhanced.length : 3)
-        .map((row, i) => (
-          <TableRow
-            key={i}
-            sx={PageStyles.tableRow}
-            onClick={() => {
-              clickedJob(row.postingId);
-            }}
-          >
-            <TableCell
-              component="th"
-              scope="row"
-              align="center"
-              style={{ paddingRight: 0 }}
-            >
-              <img
-                src={row.logo}
-                height={"40px"}
-                alt={row.companyName}
-              ></img>
-            </TableCell>
-            <TableCell>
-              <p style={PageStyles.job_title}>{row.postingTitle}</p>
-              <p style={PageStyles.company}>{row.companyName}</p>
-              <p style={PageStyles.details}>
-                {row.duration} {row.type}, {row.location}
-              </p>
-            </TableCell>
-            <TableCell align="center">
-              {StatusIcon(row.Status)}
-            </TableCell>
-          </TableRow>
-        ))}
-      <TableRow
-        key={"end"}
-        sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-      >
-       <TableCell colSpan={3} sx={PageStyles.see_more} onClick={handleSeeMoreClick}>
-          See More
-        </TableCell>
-      </TableRow>
-    </TableBody>
-  </Table>
-</TableContainer>
+          <TableContainer>
+            <Table aria-label="simple table" style={PageStyles.table}>
+              <TableBody>
+                {applicationsEnhanced.slice(0, 3).map((row, i) => (
+                  <TableRow
+                    key={i}
+                    sx={PageStyles.tableRow}
+                    onClick={() => {
+                      clickedJob(row.postingId);
+                    }}
+                  >
+                    <TableCell
+                      component="th"
+                      scope="row"
+                      align="center"
+                      style={{ paddingRight: 0 }}
+                    >
+                      <img
+                        src={row.logo}
+                        height={"40px"}
+                        alt={row.companyName}
+                      ></img>
+                    </TableCell>
+                    <TableCell>
+                      <p style={PageStyles.job_title}>{row.postingTitle}</p>
+                      <p style={PageStyles.company}>{row.companyName}</p>
+                      <p style={PageStyles.details}>
+                        {row.duration} {row.type}, {row.location}
+                      </p>
+                    </TableCell>
+                    <TableCell align="center">
+                      {StatusIcon(row.Status)}
+                    </TableCell>
+                  </TableRow>
+                ))}
+                <TableRow
+                  key={"end"}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell
+                    colSpan={3}
+                    sx={PageStyles.see_more}
+                    onClick={handleSeeMoreClick}
+                  >
+                    See More
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
 
           <p style={PageStyles.headings}>Saved Jobs</p>
-<TableContainer>
-  <Table aria-label="simple table" style={PageStyles.table}>
-    <TableBody>
-      {savedPostings
-        .slice(0, savedExpanded ? savedPostings.length : 3)
-        .map((row, i) => (
-          <TableRow
-            key={i}
-            sx={PageStyles.tableRow}
-            onClick={() => {
-              clickedJob(row.postingId);
-            }}
-          >
-            <TableCell
-              component="th"
-              scope="row"
-              align="center"
-              style={{ paddingRight: 0 }}
-            >
-              <img
-                src={row.logo}
-                height={"40px"}
-                alt={row.companyName}
-              ></img>
-            </TableCell>
-            <TableCell>
-              <p style={PageStyles.job_title}>{row.postingTitle}</p>
-              <p style={PageStyles.company}>{row.companyName}</p>
-              <p style={PageStyles.details}>
-                {row.duration} {row.type}, {row.location}
-              </p>
-            </TableCell>
-          </TableRow>
-        ))}
-  <TableRow
-  key={"end"}
-  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-  onClick={() => {
-    setPage('/jobs?tab=saved');  
-    navigate('/jobs?tab=saved');  
-  }}
-  
->
-  <TableCell
-    colSpan={3}
-    sx={PageStyles.see_more}
-  >
-    {"See More"}
-  </TableCell>
-</TableRow>
-    </TableBody>
-  </Table>
-</TableContainer>
-</Grid>
+          <TableContainer>
+            <Table aria-label="simple table" style={PageStyles.table}>
+              <TableBody>
+                {savedPostings.slice(0, 3).map((row, i) => (
+                  <TableRow
+                    key={i}
+                    sx={PageStyles.tableRow}
+                    onClick={() => {
+                      clickedJob(row.postingId);
+                    }}
+                  >
+                    <TableCell
+                      component="th"
+                      scope="row"
+                      align="center"
+                      style={{ paddingRight: 0 }}
+                    >
+                      <img
+                        src={row.logo}
+                        height={"40px"}
+                        alt={row.companyName}
+                      ></img>
+                    </TableCell>
+                    <TableCell>
+                      <p style={PageStyles.job_title}>{row.postingTitle}</p>
+                      <p style={PageStyles.company}>{row.companyName}</p>
+                      <p style={PageStyles.details}>
+                        {row.duration} {row.type}, {row.location}
+                      </p>
+                    </TableCell>
+                  </TableRow>
+                ))}
+                <TableRow
+                  key={"end"}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  onClick={() => {
+                    setPage("/jobs?tab=saved");
+                    navigate("/jobs?tab=saved");
+                  }}
+                >
+                  <TableCell colSpan={3} sx={PageStyles.see_more}>
+                    {"See More"}
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
 
         <Grid item xs={4}>
           <p style={PageStyles.headings}>Recommended</p>
@@ -258,21 +285,10 @@ export function LandingPage({ userId, setPage }) {
                         {row.duration} {row.type}, {row.location}
                       </p>
                     </TableCell>
-                    <TableCell align="center" style={{ paddingRight: 20 }}>
-                      <IconButton onClick={() => handleSave(i)}>
-                        {row.saved ? (
-                          <Bookmark
-                            height={"90px"}
-                            sx={{ color: "#ffa500", fontSize: "35px" }}
-                          />
-                        ) : (
-                          <Bookmark
-                            height={"90px"}
-                            sx={{ color: "#B8B8B8", fontSize: "35px" }}
-                          />
-                        )}
-                      </IconButton>
-                    </TableCell>
+                    <TableCell
+                      align="center"
+                      style={{ paddingRight: 20 }}
+                    ></TableCell>
                   </TableRow>
                 ))}
               </TableBody>
